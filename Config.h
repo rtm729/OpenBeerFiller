5d67bf659da2f334074b1dcbd2424a6acc731605
rtm729 <82761847+rtm729@users.noreply.github.com>
2021-04-20 09:15:41 -0400

    Update Config.h

--- Config.h
+++ Config.h
@@ -23,11 +23,11 @@
 /**
  * General Config Options
  */
-#define CO2_PURGE_PERIOD 2000
-#define MOVE_BEER_BELT_PERIOD 5000
-#define FILLER_TUBE_MOVEMENT_DELAY 2000
-#define CO2_PURGE_RETRACTION_DELAY 1000
-#define CO2_PURGE_RETRACTION_PERIOD 500
+#define CO2_PURGE_PERIOD 2000 // How long in milliseconds to purge with CO2 before filling with beer
+#define MOVE_BEER_BELT_PERIOD 5000 // How long in milliseconds to run the beer belt solenoid for, ie how long should the beer belt move.
+#define FILLER_TUBE_MOVEMENT_DELAY 2000 // How long in milliseconds to wait after raising/lowering filler tube before starting the next action like opening the filling lines.
+#define CO2_PURGE_RETRACTION_DELAY 1000 // How long in milliseconds to wait before starting CO2 purge after starting to retract filler tubes.
+#define CO2_PURGE_RETRACTION_PERIOD 500 // How long in milliseconds to purge with CO2 after filling with beer, this happes mid lifting the filler tubes
 #define FILL_SENSORS_TIMER_FREQUENCY 100000 // 100ms This value needs to be defined in microseconds.
 #define FILL_SENSORS_TRIGGER 400 // Int between 0 and 1023 used to trigger the fill sensor: operating voltage(5v or 3.3v) / 1024
 #define VARIABLE_FILL_SENSOR_TRIGGER false // Use a potentiometer to adjust trigger value
