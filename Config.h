0937331b577337b3208537f29cc4ede3f6efb542
rtm729 <82761847+rtm729@users.noreply.github.com>
2021-04-20 20:35:01 -0400

    Updated for 2 fillers added fill sensor POT

--- Config.h
+++ Config.h
@@ -3,6 +3,7 @@
  * Copyright (c) 2020 Gerhard Potgieter [https://gerhardpotgieter.com]
  *
  * Based on an idea by Christopher Harrison-Hawkes [https://harrisonsbrewery.com]
+ * 
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
